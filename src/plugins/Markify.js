a4df7748d4d3e7d03786151f1b18075557d185cc
Alexey Elizarov <felex-ae@ya.ru>
2020-09-10 20:29:31 +0300

    fix: hashtag regexp 2

--- src/plugins/Markify.js
+++ src/plugins/Markify.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-cond-assign,flowtype/require-valid-file-annotation */
 
 const MENTION_RX = /(?:^|[^\w/\\])(@[a-z][a-z0-9.-]+[a-z0-9])/;
-const TAG_RX = /#[^\s!@#$%^&*()=+.\/,\[{\]};:'"?><]+/;
+const TAG_RX = /(?:^|[^\w/\\])(#[^\s!@#$%^&*()=+.\/,\[{\]};:'"?><]+)/;
 
 export default () => ({
   onChange: (editor, next) => {
